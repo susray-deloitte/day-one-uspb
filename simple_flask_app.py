02db402bc6df3c66cc12bf56092fe84ade38b37a
RAY <susray@deloitte.com>
2025-05-21 15:09:32 +0530

    chore(jinja): added jinja template rendering example

--- simple_flask_app.py
+++ simple_flask_app.py
@@ -1,6 +1,7 @@
 # This is a simple Flask application
 
-from flask import Flask
+from flask import Flask, render_template
+import datetime
 import logging
 
 logger = logging.getLogger(__name__)
@@ -11,6 +12,11 @@ class MyApp:
     def __init__(self, name = None, info= None):
         self.name = name if name else "Demo App"
         self.info = info if info else "This is a simple Flask application."
+        self.data = {
+            "title": "Jinja Demo",
+            "app_name": self.name,
+            "current_time": datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S"),
+        }
         self.setup_routes()
 
     def setup_routes(self):
@@ -22,6 +28,11 @@ class MyApp:
         @app.get('/about')    
         def about():
             return f"About this app:<br>{self.info}"
+        
+        # Function to demonstrate Jinja2 template rendering
+        @app.get('/jinja')
+        def jinja_demo():
+            return render_template('index.html', **self.data)
     
     def run(self):
         try:
